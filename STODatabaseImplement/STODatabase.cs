d2ed943357acebef4e0d180a2321a624445e3b86
Евгений Сергеев <alexevgenii@mail.ru>
2022-04-04 21:38:06 +0400

    add dbcontext

--- STODatabaseImplement/STODatabase.cs
+++ STODatabaseImplement/STODatabase.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using STODatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace STODatabaseImplement
 {
-    public class STODatabase
+    public class STODatabase : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-Q32HI5N\SQLEXPRESS;Initial Catalog=AbstractFoodDeliveryDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<CarSparePart> CarSpareParts { get; set; }
+        public virtual DbSet<ServiceRecord> ServiceRecords { get; set; }
+        public virtual DbSet<SparePart> SpareParts { get; set; }
+        public virtual DbSet<TimeOfWork> TimeOfWorks  { get; set; }
+        public virtual DbSet<TO> TOs { get; set; }
+        public virtual DbSet<TOCar> TOCars { get; set; }
+        public virtual DbSet<TOWork> TOWorks  { get; set; }
+        public virtual DbSet<Work> Works { get; set; }
+        public virtual DbSet<WorkSparePart> WorkSpareParts { get; set; }
+        public virtual DbSet<WorkTimeOfWork> WorkTimeOfWorks { get; set; }
     }
 }
