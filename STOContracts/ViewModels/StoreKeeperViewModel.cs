25e243173c180518463056cf30a9796ae6cfbbe4
Idris <vaz21701@gmail.com>
2022-04-04 23:29:58 +0400

    Добавлены роли работника и кладовщика

---
+++ STOContracts/ViewModels/StoreKeeperViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace STOContracts.ViewModels
+{
+    public class StoreKeeperViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("ФИО")]
+        public string FIO { get; set; }
+        [DisplayName("Логин")]
+        public string login { get; set; }
+
+        [DisplayName("Пароль")]
+        public string password { get; set; }
+    }
+}
--- STODatabaseImplement/Implements/SparePartStorage.cs
+++ STODatabaseImplement/Implements/SparePartStorage.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using STOContracts.StorageContracts;
+using STOContracts.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using STOContracts.BindingModels;
+using STODatabaseImplement.Models;
 
 namespace STODatabaseImplement.Implements
 {
-    internal class SparePartStorage
+    public class SparePartStorage : ISparePartStorage
     {
+        public List<SparePartViewModel> GetFullList()
+        {
+            using var context = new STODatabase();
+            return context.SpareParts
+            .Include(rec => rec.CarSpareParts)
+            .ThenInclude(rec => rec.LoanProgram)
+            .Include(rec => rec.DepositCurrencies)
+            .ThenInclude(rec => rec.Deposit)
+            .Include(rec => rec.Manager)
+            .ToList()
+            .Select(CreateModel)
+            .ToList();
+        }
+
+
     }
 }
