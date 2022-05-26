1d4d9e6e9195e6e3acc60bc3490752aef985ac18
Евгений Сергеев <alexevgenii@mail.ru>
2022-05-26 23:37:17 +0400

    fix

---
+++ STOEmployeeApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace STOEmployeeApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
